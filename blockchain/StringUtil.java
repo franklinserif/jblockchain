ca30ce2ba91f1786582b4dc7d69c33b286f5b380
franklin rodriguez <franklinserif@gmail.com>
2025-01-12 19:39:43 -0400

    chore(all): add everything

---
+++ blockchain/StringUtil.java
@@ -0,0 +1,26 @@
+package blockchain;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+
+public class StringUtil{
+    // Applies Sha256 to string and returns the result
+    public static String applySha256(String input){
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
+
