acc3f90d06579973c9e37ca885d8aeccc41ac669
franklin rodriguez <franklinserif@gmail.com>
2025-01-14 00:19:08 -0400

    feat(Block.java): add new features

--- blockchain/Block.java
+++ blockchain/Block.java
@@ -7,6 +7,7 @@ public class Block{
     public String previousHash;
     public String data; //our data will be a simple message.
     private final long timeStamp; // as number of milliseconds since 1/1/1970
+    private int nonce;
 
 
     // Block Constructor
@@ -18,9 +19,19 @@ public class Block{
     }
 
     public String calculateHash(){
-
         return StringUtil.applySha256(
-                previousHash + timeStamp + data
+                previousHash + timeStamp + nonce + data
         );
+    }
+
+    public void mineBlock(int difficulty) {
+        // Create a string with difficulty 0
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals((target))){
+            nonce++;
+            hash = calculateHash();
+        }
+
+        System.out.println("Block Mined!!: " + hash);
     }
 }
