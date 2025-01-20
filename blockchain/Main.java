9acff87e8b6014eb61323df3909e9d0637374021
franklin rodriguez <franklinserif@gmail.com>
2025-01-19 23:39:31 -0400

    style(Main.java): format code

--- blockchain/Main.java
+++ blockchain/Main.java
@@ -1,6 +1,7 @@
 package blockchain;
 
 import java.util.ArrayList;
+
 import com.google.gson.GsonBuilder;
 
 public class Main {
@@ -14,11 +15,11 @@ public class Main {
         System.out.println("trying to Mine block 1");
         blockchain.get(0).mineBlock(difficulty);
 
-        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).hash));
         System.out.println("Trying to Mine block 2... ");
         blockchain.get(1).mineBlock(difficulty);
 
-        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("Hey im the third block", blockchain.get(blockchain.size() - 1).hash));
         System.out.println("Trying to Mine block 3... ");
         blockchain.get(2).mineBlock(difficulty);
 
@@ -35,23 +36,23 @@ public class Main {
         String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         // loop through blockchain to check hashes
-        for(int i= 1; i< blockchain.size(); i++) {
+        for (int i = 1; i < blockchain.size(); i++) {
             currentBlock = blockchain.get(i);
-            previousBlock = blockchain.get(i-1);
+            previousBlock = blockchain.get(i - 1);
             // compare registered hash and  calculated hash
 
-            if(!currentBlock.hash.equals(currentBlock.calculateHash())){
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
                 System.out.println("Current hashes not equal");
 
                 return false;
             }
 
-            if(!previousBlock.hash.equals(currentBlock.previousHash)){
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
                 System.out.println("Previous hashes not equal");
                 return false;
             }
 
-            if(!currentBlock.hash.substring(0, difficulty).equals(hashTarget)){
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
                 System.out.println("This block hasn't been mined");
                 return false;
             }
