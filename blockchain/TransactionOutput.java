5ebd2489e732eda71549fb48a65de1200ab2b5d9
franklin rodriguez <franklinserif@gmail.com>
2025-01-21 22:58:47 -0400

    feat(TransactionOutput): add full implemantention

--- blockchain/TransactionOutput.java
+++ blockchain/TransactionOutput.java
@@ -1,4 +1,22 @@
 package blockchain;
 
+import java.security.PublicKey;
+
 public class TransactionOutput {
+    public String id;
+    public PublicKey recipient; // also knows as the new owner of the coins.
+    public float value;
+    public String parentTransactionId;
+
+    //Constructor
+    public TransactionOutput(PublicKey recipient, float value, String parentTransactionId) {
+        this.recipient = recipient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256(StringUtil.getStringFromKey(recipient));
+    }
+
+    public boolean isMine(PublicKey publickey) {
+        return (publickey == recipient);
+    }
 }
